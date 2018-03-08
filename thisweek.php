d63bf8df3960f15c8e0066bc474ec0fd59349584
綿貫直志 <kosei.halfmoon@gmail.com>
2018-03-08 21:41:02 +0900

    Upgrade to a newer version.

--- thisweek.php
+++ thisweek.php
@@ -1,25 +1,30 @@
 <?php
 session_start();
   /*======================================================================+
-   | PHP version 4.4.2                                                    |
+   | PHP version 5.6.30                                                   |
    +----------------------------------------------------------------------+
    | Copyright (C) 2008.01.21 N.watanuki                                  |
    +----------------------------------------------------------------------+
    | Script-ID      : thisweek.php                                        |
    | DATA-WRITTEN   : 2008.01.21                                          |
    | AUTHER         : N.WATANUKI                                          |
-   | UPDATE-WRITTEN : ____.__.__                                          |
+   | UPDATE-WRITTEN : 2018.03.08 Upgrade to a newer version.              |
    +======================================================================*/
-require_once("sschk.php");
-$thisMonday = day_add($thisMonday,$days);
-header("Location: wcalen.php");
-$flag = 1;
-function day_add($thisMonday,$dayAdd) {
-    $defaultDay = getdate($thisMonday);
-    $year = $defaultDay["year"];
-    $month = $defaultDay["mon"];
-    $day = $defaultDay["mday"] + $dayAdd;
-    //$flag = 1;
-    return (mktime(0,0,0,$month,$day,$year));
-}
+    require_once("sschk.php");
+     
+    if (isset($_GET["days"])) {
+        $days = $_GET["days"];
+    }
+ 
+    $_SESSION["thisMonday"] = day_add($_SESSION["thisMonday"],$days);
+    $_SESSION["flag"] = 1;
+    header("Location: wcalen.php");
+
+    function day_add($thisMonday,$dayAdd) {
+        $defaultDay = getdate($thisMonday);
+        $year = $defaultDay["year"];
+        $month = $defaultDay["mon"];
+        $day = $defaultDay["mday"] + $dayAdd;
+        return (mktime(0,0,0,$month,$day,$year));
+    }
 ?>
