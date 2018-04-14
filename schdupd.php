b6954ec18b9f27a1f5be8c654ff1b88db51782c1
綿貫直志 <kosei.halfmoon@gmail.com>
2018-04-14 13:21:31 +0900

    Upgrade to a newer version.

--- schdupd.php
+++ schdupd.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
   /*======================================================================+
-   | PHP version 4.4.2                                                    |
+   | PHP version 5.6.30                                                   |
    +----------------------------------------------------------------------+
    | Copyright (C) 2002.07.29 N.watanuki                                  |
    +----------------------------------------------------------------------+
@@ -9,6 +9,7 @@ session_start();
    | DATA-WRITTEN   : 2002.07.29                                          |
    | AUTHER         : N.WATANUKI                                          |
    | UPDATE-WRITTEN : 2011.04.08                                          |
+   | UPDATE-WRITTEN : 2018.03.15 Update to a newer version.               |
    +======================================================================*/
     require_once("sschk.php"); 
     require_once("db_connect.php");
