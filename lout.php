f163a05ff5f0929dea30f351abf45eab958dee65
綿貫直志 <kosei.halfmoon@gmail.com>
2018-04-14 12:58:27 +0900

    Upgrade to a newer version.

--- lout.php
+++ lout.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
 /* +======================================================================+
-   | PHP version 4.4.2                                                    |
+   | PHP version 5.6.30                                                   |
    +----------------------------------------------------------------------+
    | Copyright (C) 2002.07.16 N.watanuki                                  |
    +----------------------------------------------------------------------+
    | Script-ID      : lout.php                                            |
    | DATA-WRITTEN   : 2002.07.16                                          |
    | AUTHER         : N.WATANUKI                                          |
-   | UPDATE-WRITTEN : ____.__.__                                          |
+   | UPDATE-WRITTEN : 2018.03.02 Upgrade to a newer version.              |
    +======================================================================+ */
     require_once("sschk.php");
     session_destroy();
