6d48a600e279749ca9d80ad03f654f490f2500a9
綿貫直志 <kosei.halfmoon@gmail.com>
2018-04-14 12:40:22 +0900

    Upgrade to a newer version.

--- loginsub.php
+++ loginsub.php
@@ -1,7 +1,7 @@
 <?php
 session_start();         
   /*======================================================================+
-   | PHP version 4.4.2                                                    |
+   | PHP version 5.6.30                                                   |
    +----------------------------------------------------------------------+
    | Copyright (C) 2002.07.16 N.watanuki                                  |
    +----------------------------------------------------------------------+
@@ -9,6 +9,7 @@ session_start();
    | DATA-WRITTEN   : 2002.07.16                                          |
    | AUTHER         : N.WATANUKI                                          |
    | UPDATE-WRITTEN : 2011.04.05                                          |
+   | UPDATE-WRITTEN : 2018.0302 Upgrade to a newer version.               |
    +======================================================================*/
     require_once("db_connect.php");
     require_once("footer.php"); 
