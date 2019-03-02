c30bfaa43d2e047167666596b86496dcae3e6069
watanukinaoshi <watanuki@NWSystems.local>
2019-03-02 20:25:48 +0900

    Upgrade to PHP version 7.1.16

--- db_connect.php
+++ db_connect.php
@@ -1,22 +1,27 @@
 <?php
   /*======================================================================+
-   | PHP version 5.6.30                                                   |
+   | PHP version 7.1.16                                                   |
    +----------------------------------------------------------------------+
-   | Copyright (C) 2011.04.05 N.watanuki                                  |
+   | Copyright (C) 2018.07.25 _.________                                  |
    +----------------------------------------------------------------------+
    | Script-ID      : db_connect.php                                      |
-   | DATA-WRITTEN   : 2011.04.05                                          |
-   | AUTHER         : N.WATANUKI                                          |
-   | UPDATE-WRITTEN : 2018.03.12 Upgrade to a newer version.              |
+   | DATA-WRITTEN   : 2019.03.02                                          |
+   | AUTHER         : _.________                                          |
+   | UPDATE-WRITTEN : ____.__.__                                          |
    +======================================================================*/
     //MySQL DB Connect
-    //define("DBSV", "localhost");
     define("DBSV", "127.0.0.1");
     define("DBNAME", "gschedule");
     define("DBUSER", "nao");
     define("DBPASS", "naow4583");
 
+    /*
     $conn = mysql_connect(DBSV, DBUSER, DBPASS) or 
         die("Error: Could not connect to Database!: " . mysql_error($conn));
     mysql_select_db(DBNAME, $conn);
+    */
+    $conn = mysqli_connect(DBSV,DBUSER,DBPASS,DBNAME);
+    if(mysqli_connect_error()) {
+        die("Error: DBへの接続に失敗しました！" . mysqli_error($conn));
+    }
 ?>
