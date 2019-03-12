27d116c092f597b68a96cf37d48d0d8fbb1e97bb
Naoshi Watanuki <nao-wata@primagest.co.jp>
2019-03-12 19:06:59 +0900

    Upgrade to PHP version 7.1.16

--- footerP.php
+++ footerP.php
@@ -15,14 +15,16 @@
        print("<DIV id='footer-center'>");
        print("<PRE>");
        print("Phrase Separator System Version 2.2.4 \n");
-       print("Copyright(C.) 2014 <A HREF='mailto:naoshi01.watanuki@g.softbank.co.jp'><U>Softbank Co.Ltd.,</U></A> Allright Reserved.");
+       // print("Copyright(C.) 2014 <A HREF='mailto:naoshi01.watanuki@g.softbank.co.jp'><U>Softbank Co.Ltd.,</U></A> Allright Reserved.");
+       print("Copyright(C.) 2019 <A HREF='mailto:nao-wata@primagest.co.jp'><U>Primagest.,Inc</U></A> Allright Reserved.");
        print("</PRE> \n");
        print("</DIV> \n");
    } else {
        print("<DIV id='footer-center'>");
        print("<PRE class='c5'>");
        print("Phrase Separator System Version 2.2.4 \n");
-       print("Copyright(C.) 2014 <A HREF='mailto:naoshi01.watanuki@g.softbank.co.jp'><U>Softbank Co.Ltd.,</U></A> Allright Reserved.");
+       // print("Copyright(C.) 2014 <A HREF='mailto:naoshi01.watanuki@g.softbank.co.jp'><U>Softbank Co.Ltd.,</U></A> Allright Reserved.");
+       print("Copyright(C.) 2019 <A HREF='mailto:nao-wata@primagest.co.jp'><U>Primagest.,Inc</U></A> Allright Reserved.");
        print("</PRE> \n");
        print("</DIV> \n");
    }
