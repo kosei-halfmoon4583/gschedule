30ebea7807e4248a88b605e72072adc5ee646e6c
綿貫直志 <watanuki@NA18502.local>
2019-03-12 16:21:54 +0900

    Upgrade to PHP version 7.1.16

--- getHotel.php
+++ getHotel.php
@@ -1,5 +1,14 @@
 <?php
-header("Content-Type: text/xml");
-$handle = @readfile("http://jws.jalan.net/APIAdvance/HotelSearch/V1/?key=peg114fd81bc17&s_area=".($_GET["s_area"]));
-/*$handle = @readfile("http://jws.jalan.net/APICommon/AreaSearch/V1/?key=peg114fd81bc17&s_area=".($_GET["s_area"])); */
+    /*==========================================================================+
+     | PHP version 7.1.16                                                       |
+     +--------------------------------------------------------------------------+
+     | Copyright (C) 2014.05.30 N.watanuki                                      |
+     +--------------------------------------------------------------------------+
+     | Script-ID      : getHotelP.php                                           |
+     | DATA-WRITTEN   : 2014.05.30                                              |
+     | AUTHER         : N.WATANUKI                                              |
+     | UPDATE-WRITTEN : 2019.03.12                                              |
+     +==========================================================================*/
+    header("Content-Type: text/xml");
+    $handle = @readfile("http://jws.jalan.net/APIAdvance/HotelSearch/V1/?key=peg114fd81bc17&s_area=".($_GET["s_area"]));
 ?>
