95270ae0a53a3a9f40b6dfc0f9870f62ed8a5388
Naoshi Watanuki <nao-wata@primagest.co.jp>
2019-03-12 18:03:13 +0900

    Upgrade to PHP version 7.1.16

--- usrschd.php
+++ usrschd.php
@@ -1,41 +1,42 @@
 <?php
 /* +========================================================================+
-   | PHP version 5.6.30                                                     |
+   | PHP version 7.1.16                                                     |
    +------------------------------------------------------------------------+
-   | Copyright (C) 2002.08.30 L. Tortona                                    |
+   | Copyright (C) 2018.07.25 _.________                                    |
    +------------------------------------------------------------------------+
    | Script-ID      : usrschd.php                                           |
-   | DATA-WRITTEN   : 2002.09.02                                            |
-   | AUTHOR         : __________                                            |
-   | UPDATE-WRITTEN : 2007.12.09 UPDATE AUTHER: Naoshi WATANUKI.            |
-   | UPDATE-WRITTEN : 2018.02.18 Upgrade to a newer version.                |
+   | DATA-WRITTEN   : 2018.07.25                                            |
+   | AUTHOR         : _.________                                            |
+   | UPDATE-WRITTEN : 2019.03.12                                            |
    +========================================================================*/
     require_once("usrschddb.php");
     require_once("footer.php"); //footer(outer file.)
     $header_title = "[ ユーザ別予定一覧表 ]";
+    $today = array();
+    $year = "";
+    $month = "";
+    $uid = "";
 
-    //Update 2018.02.18 usetに変更
-    //Check for session information
-    /*
     if (isset($s_usrschd)) { 
-        session_unregister("s_usrschd");
+        // unset("s_usrschd");
+        unset($s_usrschd);
+        // unset($_SESSION["s_usrschd"]);
     }
     if (isset($s_old_usrschd)) { 
-        session_unregister("s_old_usrschd");
+        // unset("s_old_usrschd");
+        unset($s_old_usrschd);
+        // unset($_SESSION["s_old_usrschd"]);
     } 
     if (isset($stid)) { 
-        session_unregister("stid");
+        // unset("stid");
+        unset($stid);
+        // unset($_SESSION["stid"]);
     }
-    */
-    if (isset($s_usrschd)) { 
-        unset("s_usrschd");
-    }
-    if (isset($s_old_usrschd)) { 
-        unset("s_old_usrschd");
-    } 
-    if (isset($stid)) { 
-        unset("stid");
-    }
+
+    // URLパラメータを受け取る
+    $year = $_GET["year"];
+    $month = $_GET["month"];
+    $uid = $_GET["uid"];
 ?>
 <HTML>
 <HEAD>
@@ -46,38 +47,38 @@
 <BODY>
 <?php require_once("header.php"); ?>
 <div id="content">
-<!-- <div id="header"> -->
 <div id="menu-ad">
 <PRE style="color:#3399CC;">
- [ 広告エリア ]
+ [ 広　告 ]
 </PRE>
 </div>
 
 <div id="main3">
 <FORM method="POST">
     <TABLE border="0" width="650" height="40">
-        <TR><TD width="100" height="40"><B><FONT size="2">メンバー：</FONT></B></TD>
+        <TR><TD width="100" height="40"><B><FONT size="2">登録者：</FONT></B></TD>
 <?php
-        //Check if variables were completely passed.
-        if ( isset($uid) && isset($year) && isset($month) ) {
-            //Instantiate class for retrieval of user's data.
-            $l_objUsrSched = new usrschddb();
-            //Retrieve and display user data for the month...
-            $l_objUsrSched->getUserData($uid, $year, $month);
-        } else {
-            print ("</TABLE> \n");
-            print ("<div align='center'> \n");
-            print ("Kindly pass the correct data to access this screen. \n");
-            print ("</div> \n");
-        }
-?> 
-        </TR>
-    </TABLE>
-    <TABLE border="0" width="650" height="63">
-        <TR><TD>
-            <A href="mcalen.php"><img src="./resources/images/btn_home1_5.gif" height="15" width="40"></A>
-        </TD></TR>
-    </TABLE>
+    //Check if variables were completely passed.
+    if (isset($uid) && isset($year) && isset($month) ) {
+        //Instantiate class for retrieval of user's data.
+        $l_objUsrSched = new usrschddb();
+        //Retrieve and display user data for the month...
+        $l_objUsrSched->getUserData($uid, $year, $month);
+    } else {
+        print ("</TABLE> \n");
+        print ("<div align='center'> \n");
+        print ("Kindly pass the correct data to access this screen. \n");
+        print ("</div> \n");
+    }
+    print("</TR> \n");
+    print("</TABLE> \n");
+
+    print("<TABLE border='0' width='650' height='63'> \n");
+    print("<TR><TD> \n");
+    print("<A href='mcalen.php?year=$year&month=$month'><img src='./resources/images/btn_home1_5.gif' height='15' width='40'></A> \n");
+    print("</TD></TR> \n");
+    print("</TABLE> \n");
+?>
 </FORM>
 </div>
 </div>
