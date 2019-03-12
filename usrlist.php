9be47030e6bbbd6930d50894e6e02ca8f37edbf2
Naoshi Watanuki <nao-wata@primagest.co.jp>
2019-03-12 18:01:23 +0900

    Upgrade to PHP version 7.1.16

--- usrlist.php
+++ usrlist.php
@@ -1,101 +1,94 @@
 <?php
   /*========================================================================+
-   | PHP version 5.6.30                                                     |
+   | PHP version 7.1.16                                                     |
    +------------------------------------------------------------------------+
-   | Copyright (C) 2002.08.30                                               |
+   | Copyright (C) 2018.07.25                                               |
    +------------------------------------------------------------------------+
    | Script-ID      : usrlist.php                                           |
-   | DATA-WRITTEN   : 2002.08.30                                            |
-   | AUTHOR         : L. Tortona                                            |
-   | UPDATE-WRITTEN : 2011.04.07                                            |
-   | UPDATE-WRITTEN : 2018.03.18 Upgrade to a newer version.                |
+   | DATA-WRITTEN   : 2018.07.25                                            |
+   | AUTHOR         : _.________                                            |
+   | UPDATE-WRITTEN : 2019.03.12                                            |
    +========================================================================*/
-/*--------------------*
- * User List Class    *
- *--------------------*/
-class usrlist {
-    var $href;
-    var $content;
-    var $sesUserID;
-    /*------------------*
-     *  DB Connection   *
-     *------------------*/
-    function connectDB() { 
+    // require_once("db_connect.php");
+    /*--------------------*
+     * User List Class    *
+     *--------------------*/
+    class usrlist {
+        var $href;
+        var $content;
+        var $sesUserID;
+        /*------------------*
+         *  DB Connection   *
+         *------------------*/
+        function connectDB() {
 
-        /* define("DBSV", "localhost"); */
-        define("DBSV", "127.0.0.1");
-        define("DBNAME", "gschedule");
-        define("DBUSER", "nao");
-        define("DBPASS", "naow4583");
+            define("DBSV", "127.0.0.1");
+            define("DBNAME", "gschedule");
+            define("DBUSER", "nao");
+            define("DBPASS", "naow4583");
 
-        $conn = mysql_connect(DBSV, DBUSER, DBPASS) or 
-            die("Could not connect to Database!: " . mysql_error($conn));
-        mysql_select_db(DBNAME, $conn);
-    }
-    /*-----------------------------------------------------------------------*
-     *  Function that handles the DB retrieval of data and printing of data. *
-     *-----------------------------------------------------------------------*/
-    function getUserList() {
-    
-        print ($this->content = $this->getUserInfo());
-    
-    }
-    /*-----------------------------------------------------*
-     *  Function that retrieves userID from the database.  *
-     *-----------------------------------------------------*/
-    function getUserInfo() {
-    
-      //MySQL DB Connect
-        /* define("DBSV", "localhost"); */
-        define("DBSV", "127.0.0.1");
-        define("DBNAME", "gschedule");
-        define("DBUSER", "nao");
-        define("DBPASS", "naow4583");
-
-        $conn = mysql_connect(DBSV, DBUSER, DBPASS) or 
-            die("Could not connect to Database!: " . mysql_error($conn));
-        mysql_select_db(DBNAME, $conn);
-
-        $l_strSql = "SELECT userid, jpname FROM accounttb ORDER by jpname";
-
-        //Execute query for User ID retrieval.
-        $l_resOut = mysql_query($l_strSql, $conn);
+            $conn = mysqli_connect(DBSV,DBUSER,DBPASS,DBNAME);
+            if(mysqli_connect_error()) {
+                die("Error: DBへの接続に失敗しました！");
+            }
+        }
+        /*-----------------------------------------------------------------------*
+         *  Function that handles the DB retrieval of data and printing of data. *
+         *-----------------------------------------------------------------------*/
+        function getUserList() {
         
-        //Get Number of Rows.
-        $l_intNoRow = mysql_num_rows($l_resOut);  
+            print ($this->content = $this->getUserInfo());
         
-        //Initialize return values
-        $l_strReturnValue = "<OPTION value='  '>全　員</OPTION>\n";
+        }
+        /*-----------------------------------------------------*
+         *  Function that retrieves userID from the database.  *
+         *-----------------------------------------------------*/
+        function getUserInfo() {
+
+
+            // MySQL DB Connect
+            require_once("db_connect.php");
         
-        //Check if there are no data retrieved.
-        if ($l_intNoRow == 0) {
-            //$l_strReturnValue .= "</SELECT>";
+            $l_strSql = "SELECT userid, jpname FROM accounttb ORDER by jpname";
+
+            //Execute query for User ID retrieval.
+            $l_resOut = mysqli_query($conn, $l_strSql);
+            
+            //Get Number of Rows.
+            $l_intNoRow = mysqli_num_rows($l_resOut);  
+            
+            //Initialize return values
+            $l_strReturnValue = "<OPTION value='  '>登録ユーザ</OPTION>\n";
+            
+            //Check if there are no data retrieved.
+            if ($l_intNoRow == 0) {
+                //$l_strReturnValue .= "</SELECT>";
+                //Disconnect from the database.
+                mysqli_close($conn);
+                //Return value with no data.
+                return($l_strReturnValue);
+            }
+            //If data were retrieved perform loop to retrieve all data..
+            while ($l_objRowData = mysqli_fetch_assoc($l_resOut)) {
+             
+                //Get Data.
+                $l_strUserID   = $l_objRowData["userid"];
+                $l_strUsername = $l_objRowData["jpname"];
+                
+                //Set data onto the display window.
+                $l_strReturnValue .= "<OPTION value='";
+                $l_strReturnValue .= $l_strUserID ;
+                $l_strReturnValue .= "'>";
+                $l_strReturnValue .= $l_strUsername;
+                $l_strReturnValue .= "</OPTION>\n";
+            }
+            
             //Disconnect from the database.
-            mysql_close($conn);
-            //Return value with no data.
+            mysqli_close($conn);
+            
+            //Set end marker and return value.
+            //$l_strReturnValue .= "</SELECT>";
             return($l_strReturnValue);
         }
-        //If data were retrieved perform loop to retrieve all data..
-        while ($l_objRowData = mysql_fetch_assoc($l_resOut)) {
-         
-            //Get Data.
-            $l_strUserID   = $l_objRowData["userid"];
-            $l_strUsername = $l_objRowData["jpname"];
-            
-            //Set data onto the display window.
-            $l_strReturnValue .= "<OPTION value='";
-            $l_strReturnValue .= $l_strUserID ;
-            $l_strReturnValue .= "'>";
-            $l_strReturnValue .= $l_strUsername;
-            $l_strReturnValue .= "</OPTION>\n";
-        }
-        
-        //Disconnect from the database.
-        mysql_close($conn);
-        
-        //Set end marker and return value.
-        //$l_strReturnValue .= "</SELECT>";
-        return($l_strReturnValue);
     }
-}
 ?>
